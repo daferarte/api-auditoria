9ff142bab9bfdf63a315c53b149c7cf2e7326a75
Daniel Arteaga <dfarteaga@unicesmag.edu.co>
2022-05-18 11:59:05 -0500

    Ajuste de modelo para null

--- auditoria/AudBaseDatos/models.py
+++ auditoria/AudBaseDatos/models.py
@@ -21,8 +21,8 @@ from django.db import models
 class AuditoriaBaseDatos(models.Model):
     TableName = models.CharField(max_length=100, verbose_name='Nombre Tabla')
     Operation = models.CharField(max_length=1, verbose_name='Operación')
-    OldValue =  models.CharField(max_length=255, verbose_name='Antiguos valores', blank=True)
-    NewValue = models.CharField(max_length=255, verbose_name='Nuevos valores', blank=True)
+    OldValue =  models.CharField(max_length=255, verbose_name='Antiguos valores', blank=True, null=True)
+    NewValue = models.CharField(max_length=255, verbose_name='Nuevos valores', blank=True, null=True)
     UpdateDate = models.DateTimeField(auto_now_add=True, verbose_name='Fecha creación')
     UserName = models.CharField(max_length=70, verbose_name='Usuario')
     idtabla = models.BigIntegerField(verbose_name='idTabla')
