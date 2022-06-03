4b272885e4e1d922641d9344327adc498a5b813d
Daniel Arteaga <dfarteaga@unicesmag.edu.co>
2022-06-03 11:11:05 -0500

    inclusion del modelo admin de la auditoria

--- auditoria/AudSoftware/admin.py
+++ auditoria/AudSoftware/admin.py
@@ -1,3 +1,34 @@
+__author__ = "Daniel Arteaga"
+__copyright__ = "Copyright 2022, Auditoria"
+__credits__ = ["Daniel Arteaga"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Daniel Arteaga"
+__email__ = "dfarteaga@unicesmag.edu.co"
+__status__ = "Test"
+
 from django.contrib import admin
+from .models import AuditoriaBaseDatos, AuditoriaMoodle, AuditoriaSoftware
 
 # Register your models here.
+
+"""
+@class AudBaseDatosAdmin
+Clase que permite la manipulacion, los filtros y demas en el administrador de djando
+"""
+class AudSoftwareAdmin(admin.ModelAdmin):
+    readonly_fields = ('fecha',)
+    search_fields = ('usuario','fecha','aplicativo','accion','modulo','consulta')
+     # list_filter=('visible',)
+    list_display = ('aplicativo','usuario', 'fecha')
+    ordering = ('-fecha', )
+
+class AudMoodleAdmin(admin.ModelAdmin):
+    readonly_fields = ('fecha',)
+    search_fields = ('aplicativo','usuario','fecha')
+     # list_filter=('visible',)
+    list_display = ('usuario','aplicativo', 'fecha')
+    ordering = ('-fecha', )
+
+# Register your models here.
+admin.site.register(AuditoriaSoftware, AuditoriaMoodle, AudSoftwareAdmin, AudMoodleAdmin)
